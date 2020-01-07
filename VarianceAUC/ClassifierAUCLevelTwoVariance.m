2001c98475cfe880e7ba8cba6998b382337985cc
Weijie Chen <weijie.chen@outlook.com>
2020-01-06 22:54:01 -0500

    Update ClassifierAUCLevelTwoVariance.m

--- VarianceAUC/ClassifierAUCLevelTwoVariance.m
+++ VarianceAUC/ClassifierAUCLevelTwoVariance.m
@@ -28,8 +28,7 @@ function [auc,var_total,var_train,var_test,m]=ClassifierAUCLevelTwoVariance(h1c,
 %
 %Reference:
 %W. Chen, B. D. Gallas, W. A. Yousef, "Classifier variability: accounting
-%for training and testing," submitted to IEEE Trans PAMI
-%
+%for training and testing," Pattern Recognition, 45 (7), 2661-2671
 %Weijie Chen 03/30/2011
 %
 %@@@@@@@@@@@@@@Disclaimer@@@@@@@@@@@@@@@@@@@
